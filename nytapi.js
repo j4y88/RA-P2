819d783e98877f617cc4d371deace999c2b3c07a
j4y88 <j4y88@hotmail.com>
2017-04-23 21:22:10 -0400

    Load up with no photo skip

--- nytapi.js
+++ nytapi.js
@@ -1,50 +1,49 @@
-var apiKey =  "a5a8d9167e074a03babab4ff8bef0945";
+var theJSON = "";
+var output= "";
+var url = "https://api.nytimes.com/svc/topstories/v2/home.json?api-key=a5a8d9167e074a03babab4ff8bef0945";
+var serviceChannel = new XMLHttpRequest();
 
-function successFn(e){
-            var target= e.target;
-            var theData = target.responseText;
-            var output = "", i = 0, theJSON = "";
+serviceChannel.open('GET', url, true);
+serviceChannel.responseType = 'text';
 
-            theJSON = JSON.parse(theData);
+serviceChannel.onload = function () {
+    if (serviceChannel.readyState === serviceChannel.DONE) {
+        if (serviceChannel.status === 200) {
+            theJSON = JSON.parse(serviceChannel.responseText);
             console.log(theJSON);
-            for (i=0; i < theJSON.results.length; i++){
-              output += "<a href='" + theJSON.results[i].url + "'>";
-              output += "<img src='" + theJSON.results[i].multimedia[2].url + "'>";
-              output += theJSON.results[i].title;
-              output += "</a><br>";
+            for (var i=0; i < theJSON.results.length; i++){
+              if(theJSON.results[i].multimedia.length != 0){
+                output += "<a href='" + theJSON.results[i].short_url + "'>";
+                output += "<img src='" + theJSON.results[i].multimedia[1].url + "'>";
+                output += theJSON.results[i].title;
+                output += "</a><br>";
+              }
             }
-
-            document.getElementById("output").innerHTML=output;
+            document.getElementById("output").innerHTML=output;        
         }
-
-  function errorFn(e){
-              console.log("An Error occurred. Error is ");
-              console.log(e.target.status + " " + e.target.statusText);
-        }
-
-function processResults(e){
-  if (e.target.status==200){
-    successFn(e);
-  } else {
-    errorFn(e);
-  }
-}
-
-function onResults(e){
-      var target = e.target;
-      var readyState = target.readyState;
-      var httpStatus = target.status;
-      if(e.target.readyState==4){
-        processResults(e);
     }
 }
 
-function ready(e){
-  var serviceChannel = new XMLHttpRequest();
-  serviceChannel.addEventListener("readystatechange",onResults,false);
-  var  url = "https://api.nytimes.com/svc/topstories/v2/home.json?api-key=a5a8d9167e074a03babab4ff8bef0945";
-  serviceChannel.open("GET", url, true);
-  serviceChannel.send();
+serviceChannel.send(null);
+
+
+//Dropdown Box
+function catagoryOpen() {
+    document.getElementById("catagoryDropdown").classList.toggle("show");
 }
 
-window.addEventListener("load",ready,false);
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+//window.addEventListener("load",connectNYT,false);
